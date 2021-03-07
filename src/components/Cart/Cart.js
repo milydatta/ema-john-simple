65cfcff6fe3e0eda95251b6441e5aaac3c7c795c
mily datta <dmily270@gmail.com>
2021-03-07 22:55:50 +0600

    finished

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,13 +1,14 @@
 import React from 'react';
 
+
 const Cart = (props) => {
     const cart = props.cart;
-    console.log(cart);
+    //console.log(cart);
     //const totalPrice = cart.reduce((total,prd) => total + prd.price,0)
     let total = 0;
     for (let i = 0; i < cart.length; i++) {
         const product = cart[i];
-        total = total + product.price;
+        total = total + product.price * product.quantity;
     }
 
     let shipping = 0;
@@ -37,8 +38,12 @@ const Cart = (props) => {
            <p><small>Shipping Cost: {shipping}</small></p>
            <p><small>Tax + VAT: {tax}</small></p>
            <p>Total Price: {grandTotal} </p>
+           <br/>
+           {
+               props.children
+           }
         </div>
     );
 };
 
-export default Cart;
+export default Cart; 
